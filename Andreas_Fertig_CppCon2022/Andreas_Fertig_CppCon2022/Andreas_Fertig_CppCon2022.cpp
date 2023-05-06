68c5f228d61d51d6708caeb5f58cd94f41fdca8b
wave-rider <wave-rider@users.noreply.github.com>
2023-05-07 00:56:37 +1200

    move sem

---
+++ Andreas_Fertig_CppCon2022/Andreas_Fertig_CppCon2022/Andreas_Fertig_CppCon2022.cpp
@@ -0,0 +1,53 @@
+// Andreas_Fertig_CppCon2022.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+class Fun2
+{
+
+};
+
+void Fun(std::vector<int>& ref)
+{
+    std::cout << "ref..." << ref.at(0) << "\n";
+}
+
+void Fun(const std::vector<int>& ref)
+{
+    std::cout << "const ref...\n";
+}
+
+void Fun(const std::vector<int>&& ref)
+{
+    std::cout << "move ref...\n";
+}
+
+void Use()
+{
+    std::vector v{ 2,3,4 };
+    const std::vector cv{ 5,6,7 };
+    Fun(v); // ref will be called.
+    Fun(static_cast<std::vector<int> &&>(v));
+    Fun(cv);
+    Fun({ 3,5,6 });
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Use();
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
