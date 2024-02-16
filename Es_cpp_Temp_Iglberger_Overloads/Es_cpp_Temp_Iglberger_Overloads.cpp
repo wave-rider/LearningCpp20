3d64a847b72e2475fa69649eb83749d5c7d334fb
wave-rider <wave-rider@users.noreply.github.com>
2024-02-17 00:07:55 +1300

    templates practice

---
+++ Es_cpp_Temp_Iglberger_Overloads/Es_cpp_Temp_Iglberger_Overloads.cpp
@@ -0,0 +1,27 @@
+// Es_cpp_Temp_Iglberger_Overloads.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+void f(int) { std::puts("f(int)"); }
+void f(double) { std::puts("f(double)"); }
+void f(int, int) { std::puts("f(int, int)"); }
+
+int main()
+{
+    // f(42U); This will not work
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
