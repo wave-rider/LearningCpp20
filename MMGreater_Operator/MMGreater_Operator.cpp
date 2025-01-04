fe68a5c2d8a13b8ab8378fdc521711da7bd93bdb
wave-rider <wave-rider@users.noreply.github.com>
2025-01-04 22:46:00 +1300

    MMGreater_Operator

---
+++ MMGreater_Operator/MMGreater_Operator.cpp
@@ -0,0 +1,24 @@
+// MMGreater_Operator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+//After reading Hidden Features and Dark Corners of C++ / STL on comp.lang.c++.moderated, I was completely surprised that the following snippet compiled and worked in both Visual Studio 2008 and G++ 4.4.I would assume this is also valid C since it works in GCC as well.
+
+//Here's the code:
+
+#include <stdio.h>
+int main()
+{
+    int x = 10;
+    //while (x--> 0) // x goes to 0
+    //{
+    //    printf("%d ", x);
+    //}
+    while ( x--\
+                \
+                 \
+                  \
+                   > 0)
+        printf("%d ", x);
+}
